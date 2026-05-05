27de133c1d27359397d73aeb743896ebe2d96d44
Nicu Siderias <nicu.siderias@analog.com>
2026-05-05 15:33:54 +0300

    Update create_debian.sh

--- .github/scripts/create_debian.sh
+++ .github/scripts/create_debian.sh
@@ -14,18 +14,11 @@ $SUDO apt-get update
 $SUDO apt-get install -y build-essential make devscripts debhelper
 
 #Replace placeholders inside the debian template files
-sed -i "s/@VERSION@/$version-1/" packaging/debian/changelog
+sed -i "s/@VERSION@/$version/" packaging/debian/changelog
 sed -i "s/@DATE@/$(date -R)/" packaging/debian/changelog
 
 cp -r packaging/debian .
 
 rm -rf packaging
 
-pushd ..
-tar czf ${source_code}_${version}.orig.tar.gz \
-    --exclude='.git' \
-    --exclude='debian' \
-    $source_code
-popd
-
-debuild
+debuild -us -uc
